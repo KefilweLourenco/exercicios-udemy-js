a0d63438ea4376d1d3cbd8ecbf9832f161cfd356
Kefilwe Lourenco <kefilwebr@hotmail.com>
2026-02-22 19:49:21 -0300

    Adicionando pastas de variáveis, convenção e reservada

---
+++ 3_Estruturas_de_Programacao/3_convencao/scripts.js
@@ -0,0 +1,8 @@
+// let "3nome" = 'teste'  O numero não pode iniciar a variavel
+let nome3 = "teste";
+let $nome = "teste 2";
+let _nome = "teste3";
+// let @nome = 'teste';  Nem iniciar com @
+// let !nome = 'teste';  Nem iniciaar com !
+let Nome = "teste";
+let seuNome = "teste";
---
+++ 3_Estruturas_de_Programacao/4_reservadas/scripts.js
@@ -0,0 +1,9 @@
+// palavras reservadas não podem ser
+// usadas para criar variáveis
+// let if = 'teste';
+// let funcion = 'teste';
+let functionTest = "teste";
+let function1 = "teste2";
+
+console.log(functionTest);
+console.log(function1);
